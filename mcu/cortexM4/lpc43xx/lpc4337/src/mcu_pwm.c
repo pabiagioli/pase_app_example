44e153fdbeff5240c3f2e0907eb6d86bf8cb30e1
Pablo Biagioli <pabiagioli@gmail.com>
2017-07-07 18:59:57 -0300

    initial commit. mcu layer and copied application6 sample app.

---
+++ mcu/cortexM4/lpc43xx/lpc4337/src/mcu_pwm.c
@@ -0,0 +1,37 @@
+/*
+ * mcu_pwm.c
+ *
+ *  Created on: Jul 2, 2017
+ *      Author: pampa
+ */
+
+#include "mcu_pwm.h"
+#include "chip.h"
+
+
+extern void mcu_pwm_init(void){
+
+	/* Source: https://www.lpcware.com/content/faq/how-use-sct-standard-pwm-using-lpcopen */
+   /* Initialize the SCT as PWM and set frequency */
+   Chip_SCTPWM_Init(LPC_SCT);
+}
+
+extern void configPWM(pwm_channel channel, uint32_t freq){
+
+    Chip_SCTPWM_Init(LPC_SCT);
+
+    Chip_SCTPWM_SetRate(LPC_SCT, freq);
+
+    Chip_SCU_PinMux(SCTdataList[channel].port, SCTdataList[channel].pin, SCU_MODE_FUNC1, FUNC1);
+
+    Chip_SCTPWM_SetOutPin(LPC_SCT, 1, channel);
+
+}
+
+extern void mcu_pwm_start(void){
+	Chip_SCTPWM_Start(LPC_SCT);
+}
+
+extern void mcu_pwm_setDutyCicle(uint32_t duty){
+	Chip_SCTPWM_SetDutyCycle(LPC_SCT,1,duty);
+}
