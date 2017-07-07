648e0c881c4c5bf71309fc4d9785a3707a64ab2f
Leandro Soria <leandrosoria@hotmail.com>
2017-07-07 18:27:33 -0300

    agregado mcu_pwm.h

---
+++ mcu/cortexM4/lpc43xx/lpc4337/inc/mcu_pwm.h
@@ -0,0 +1,30 @@
+#ifndef MCU_PWM_H
+#define MCU_PWM_H
+
+/*==================[inclusions]=============================================*/
+#include "stdbool.h"
+#include "stdint.h"
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+}
+#endif
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef MCU_GPIO_H */
+
